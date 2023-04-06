a593adc7fda48846835599f4a7db8c5a69c8e1af
kurmanzhan <erkinovakurmanjan960@gmail.com>
2023-04-06 17:38:01 +0600

    styled button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,17 +1,45 @@
 import { Button as ReusableButton } from '@mui/material'
 import React from 'react'
+import { styled } from '@mui/material/styles'
 
 const Button = ({ children, variant, disabled, onClick, ...rest }) => {
    return (
-      <ReusableButton
+      <ButtonStyled
          disabled={disabled}
          variant={variant}
          onClick={onClick}
          {...rest}
       >
          {children}
-      </ReusableButton>
+      </ButtonStyled>
    )
 }
 
 export default Button
+const ButtonStyled = styled(ReusableButton)(() => ({
+   '&': {
+      fontFamily: 'Manrope,sans-serif',
+      padding: '14px 67px',
+      lineHeight: '16px',
+      fontSize: '12px',
+      color: '#FFFFFF',
+      fontWeight: '600',
+      width: '191px',
+      background: 'linear-gradient(180.61deg, #0CBB6B 0.45%, #027B44 99.39%);',
+      borderRadius: '10px',
+      letterSpacing: ' 0.03em',
+   },
+
+   '&:hover': {
+      background: 'linear-gradient(180.61deg, #08DF7D 0.45%, #048F50 82.76%);',
+      color: '#fff',
+   },
+   '&:active': {
+      background: 'linear-gradient(180.61deg, #0CBB6B 0.45%, #027B44 99.39%)',
+      color: '#FFFF',
+   },
+   '&:disabled': {
+      background: '#D3D3D3',
+      color: '#FFFF',
+   },
+}))
