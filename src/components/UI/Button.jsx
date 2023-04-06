8fd0e5c2dce72b78620849a456fc1da329c32640
kurmanzhan <erkinovakurmanjan960@gmail.com>
2023-04-06 16:14:28 +0600

    add ...rest

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,9 +1,14 @@
 import { Button as ReusableButton } from '@mui/material'
 import React from 'react'
 
-const Button = ({ children, variant, disabled, onClick }) => {
+const Button = ({ children, variant, disabled, onClick, ...rest }) => {
    return (
-      <ReusableButton disabled={disabled} variant={variant} onClick={onClick}>
+      <ReusableButton
+         disabled={disabled}
+         variant={variant}
+         onClick={onClick}
+         {...rest}
+      >
          {children}
       </ReusableButton>
    )
