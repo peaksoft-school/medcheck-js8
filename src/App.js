31be46a831630572a9a732c90f542de65825ced0
Abdybakieva <aselabdybakievaa@exampele.com>
2023-04-06 11:06:50 +0600

    done checkbox and switcher

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
-import CheckboxApp from './components/UI/checkbox/Checkbox'
-import SwitchApp from './components/UI/switch/Switch'
-
 function App() {
-   return (
-      <>
-         <div>Med Check</div>
-         <CheckboxApp />
-         <SwitchApp />
-      </>
-   )
+   return <div>Med Check</div>
 }
 export default App
