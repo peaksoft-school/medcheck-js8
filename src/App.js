5fc539bea8fc6a5754d9983e4f3be2fa38b255f9
RussoOrmon <115459787+RussoOrmon@users.noreply.github.com>
2023-04-11 23:03:59 +0300

    empty modal window

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
 function App() {
-   return <div>Med Check</div>
+   return <div>Med Check </div>
 }
 export default App
--- src/components/UI/ModalUi.jsx
+++ src/components/UI/ModalUi.jsx
@@ -2,102 +2,67 @@ import * as React from 'react'
 import {
    styled,
    Dialog,
-   Button,
    DialogActions,
    DialogContent,
    DialogContentText,
    DialogTitle,
 } from '@mui/material'
-import IconButton from '@mui/material/IconButton'
-import { ReactComponent as CloseIcon } from '../../assets/icons/closeIcon.svg'
 
 export const ModalUi = (props) => {
-   const {
-      open,
-      onClose,
-      onClick,
-      contentText,
-      titleText,
-      mainText,
-      FormInput,
-   } = props
+   const { open, onClose } = props
    return (
-      <div>
-         <StyledModalDialog open={open} onClose={onClose}>
-            <IconButton
-               aria-label="close"
-               onClick={onClose}
+      <StyledModalDialog open={open} onClose={onClose}>
+         <StyledModal>
+            <DialogTitle
                sx={{
-                  position: 'absolute',
-                  right: 8,
-                  top: 8,
-                  color: '#959595',
+                  fontSize: '36px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  fontFamily: 'Manrope',
                }}
+               id="alert-dialog-title"
             >
-               <CloseIcon />
-            </IconButton>
-            <StyledModal>
-               <DialogTitle
-                  sx={{
-                     fontSize: '36px',
-                     display: 'flex',
-                     justifyContent: 'center',
-                     fontFamily: 'Manrope',
-                  }}
-                  id="alert-dialog-title"
-               >
-                  {/* this could be the title text */}
-                  {titleText || null}
-               </DialogTitle>
-               <DialogTitle
-                  sx={{
-                     fontSize: '28px',
-                     display: 'flex',
-                     justifyContent: 'center',
-                     fontFamily: 'Manrope',
-                  }}
-                  id="alert-dialog-main"
-               >
-                  {/* this could be the main text */}
-                  {mainText || null}
-               </DialogTitle>
-               <DialogContent>
-                  <DialogContentText id="alert-dialog-content" />
-                  <StyledContentText>
-                     {/* this could be content text */}
-                     {contentText || null}
-                  </StyledContentText>
-               </DialogContent>
-               <StyledForm>
-                  {/* there could be input forms here */}
-                  {FormInput || null}
-                  {FormInput || null}
-               </StyledForm>
-               <DialogActions>
-                  {/* there might be a button here */}
-                  <Button fullWidth onClick={onClick}>
-                     Отменить
-                  </Button>
-                  <Button fullWidth onClick={onClose} autoFocus>
-                     Удалить
-                  </Button>
-               </DialogActions>
-            </StyledModal>
-         </StyledModalDialog>
-      </div>
+               {/* this could be the title text */}
+            </DialogTitle>
+            <DialogTitle
+               sx={{
+                  fontSize: '28px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  fontFamily: 'Manrope',
+               }}
+               id="alert-dialog-main"
+            >
+               {/* this could be the main text */}
+            </DialogTitle>
+            <DialogContent>
+               <DialogContentText id="alert-dialog-content" />
+               <StyledContentText>
+                  {/* this could be content text */}
+               </StyledContentText>
+            </DialogContent>
+            <StyledForm>{/* there could be input forms here */}</StyledForm>
+            <DialogActions>{/* there might be a button here */}</DialogActions>
+         </StyledModal>
+      </StyledModalDialog>
    )
 }
 
 const StyledModalDialog = styled(Dialog)(() => ({
    boxSizing: 'border-box',
-   background: '#5f575743',
+   background: '#04030343',
    fontFamily: 'Manrope',
 
    '& .MuiDialog-paper': {
       borderRadius: '16px',
       maxWidth: '659px',
+      minWidth: '303px',
+      minHeight: '284px',
+      maxHeight: '468px',
+      background: '#EBF2FC',
    },
 }))
+
 const StyledContentText = styled('div')(() => ({
    fontFamily: 'Manrope',
    fontSize: '18px',
