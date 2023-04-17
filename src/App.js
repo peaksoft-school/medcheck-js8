8940712ad201a1efcbf59026f5e8354d1c1f223d
Gulli <gulli301185@gmail.com>
2023-04-17 14:24:59 +0600

    fixing images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import Service from './components/service/Service'
-
 function App() {
-   return (
-      <div>
-         <Service />
-      </div>
-   )
-   // return <div>MedCheck</div>
+   return <div>MedCheck</div>
 }
 
 export default App
