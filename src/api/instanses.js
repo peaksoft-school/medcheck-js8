8e908b54bccd2407bb5a39ffac0645fc9c6796d3
bayaman77 <bayaman.srazhidinov7@gmail.com>
2023-05-19 11:31:34 +0600

    changed base url

--- src/api/instanses.js
+++ src/api/instanses.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { store } from '../redux/store'
 
 export const mainApi = axios.create({
-   baseURL: 'http://ec2-52-59-249-63.eu-central-1.compute.amazonaws.com',
+   baseURL: 'http://ec2-52-58-184-19.eu-central-1.compute.amazonaws.com',
 })
 
 mainApi.interceptors.request.use(
